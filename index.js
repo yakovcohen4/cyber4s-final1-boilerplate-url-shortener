6081d4c36abbb195cc96f048dd3d9eadf09eb494
yakovcohen4 <yakovc1431@gmail.com>
2021-11-03 13:25:45 +0200

    changes (short id, remove file app)

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const app = require("./app");
 const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.use("/", express.static(`./front/dist`));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/front/dist/index.html");
+});
+
+const urlsRouters=require("./back/routers/urlRouter")
+app.use("/api" , urlsRouters)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
