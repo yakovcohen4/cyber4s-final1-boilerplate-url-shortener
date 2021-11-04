16797571dc5bb7c16cf579d0d7b15c362aaf38fc
yakovcohen4 <yakovc1431@gmail.com>
2021-11-04 13:33:05 +0200

    add middleware in the index.js

--- index.js
+++ index.js
@@ -4,17 +4,31 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+const urlsRouter = require("./back/routers/urlRouter");
+const { urlHandler } = require("./back/middleware/urlHandler");
+const { errorHandler } = require("./back/middleware/errorHandler");
 
+// add cors app
+app.use(cors({
+  origin: "*"
+}));
+
+app.use(express.json())
 app.use("/", express.static(`./front/dist`));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/front/dist/index.html");
 });
 
-const urlsRouters=require("./back/routers/urlRouter")
-app.use("/api" , urlsRouters)
+// error url
+app.use(urlHandler)
 
+app.use("/api" , urlsRouter)
+
+// error hadler
+app.use(errorHandler)
+
+// Listening on port
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
