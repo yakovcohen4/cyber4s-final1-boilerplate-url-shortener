2105c64243e5497a291d7f438ced2801b4db6c24
yakovcohen4 <yakovc1431@gmail.com>
2021-11-04 19:18:35 +0200

    add nice css and copy url fun

--- front/app/main.js
+++ front/app/main.js
@@ -1,21 +1,70 @@
 // import "./styles/index.scss";
 import "./styles/style.css";
-document.getElementById("submit").addEventListener("click", shortApi);
+
+const shortbtn = document.getElementById("submit")
+shortbtn.addEventListener("click", shortApi);
+
 const div = document.getElementById("shorturl");
+const divLoader = document.getElementById("loader");
 
 async function shortApi(){
-    let longUrl = document.getElementById("url_input").value;
-    div.style.display ="block";
+    const longUrl = document.getElementById("url_input");
+    div.style.display = "block";
 
     try{
         let response = await axios.post("http://localhost:3000/api",{
-            "longUrl":longUrl
+            "longUrl":longUrl.value
         })
-        console.log(response.data);
-        div.innerText = "your new url:" + "  " + (response.data);
-        
+
+        newURL(response.data);
+        longUrl.value = "";
     }
     catch(error){
-        document.getElementById("shorturl").innerText = (error.response.data.error);
+        longUrl.value = "";
+        errormessage(error)
     }
+}
+
+// error fun 
+function newURL(response) {
+
+    const newURL = document.createElement('div');
+    newURL.setAttribute("id", "newurl");
+    newURL.classList.add("errorMessage");
+    newURL.textContent = `${response}`;
+    divLoader.appendChild(newURL)
+    
+    const copyBtn = document.createElement("button");
+    copyBtn.classList.add('close-loader-btn');
+    copyBtn.textContent = "copy";
+
+    copyBtn.addEventListener("click", ()=>{
+        const copyText = document.getElementById("newurl").textContent; // text
+        const url = copyText.split('copy')[0];
+        navigator.clipboard.writeText(url);                             // copy url
+    });
+    newURL.appendChild(copyBtn);
+
+}
+
+// error fun 
+function errormessage(error) {
+
+    const errorMessage = document.createElement('div')
+    errorMessage.classList.add("errorMessage");
+    errorMessage.textContent = error.response.data.error;
+    divLoader.appendChild(errorMessage)
+    
+    const closeBtn = document.createElement("button");
+    closeBtn.classList.add('close-loader-btn')
+    closeBtn.textContent = "close";
+    closeBtn.addEventListener("click", ()=> {
+        document.querySelector(".errorMessage").remove()
+    });
+    errorMessage.appendChild(closeBtn);
+    
+    setTimeout(()=>{
+        divLoader.removeChild(errorMessage)
+    },4000)
+    
 }
